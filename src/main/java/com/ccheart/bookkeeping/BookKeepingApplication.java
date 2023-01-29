2f81bca766e9b055b9601e87665a43133fdfca6a
cl <clforemail@163.com>
2023-01-29 11:25:14 +0800

    test

---
+++ src/main/java/com/ccheart/bookkeeping/BookKeepingApplication.java
@@ -0,0 +1,11 @@
+package com.ccheart.bookkeeping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookKeepingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookKeepingApplication.class, args);
+    }
+}
